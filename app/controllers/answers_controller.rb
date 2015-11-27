c0587b142a44e410404976da2dd6d2aefb6dabd1
Rim-777 <javatimomoss@gmail.com>
2015-11-27 10:13:16 +0200

    Сделал отдельный паршл на добавление вопроса с использованием ajax

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,5 @@
 class AnswersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:create, :destroy]
   before_action :set_question, only: :create
 
 
