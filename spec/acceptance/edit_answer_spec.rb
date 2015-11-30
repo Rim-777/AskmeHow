3e7b8fbe946a794fdfa9c9120941596246c15a78
Rim-777 <javatimomoss@gmail.com>
2015-11-30 22:02:29 +0200

    Add some test

--- spec/acceptance/edit_answer_spec.rb
+++ spec/acceptance/edit_answer_spec.rb
@@ -3,9 +3,9 @@ require_relative 'acceptance_helper'
 feature 'Edit Answer', %q{
 In order to correct mistakes in my answer
 I want to be able to edit my answers} do
-  given!(:author_of_question) { create(:user) }
-  given!(:author_of_answer) { create(:user) }
-  given!(:another_authenticated_user) { create(:user) }
+  given(:author_of_question) { create(:user) }
+  given(:author_of_answer) { create(:user) }
+  given(:another_authenticated_user) { create(:user) }
 
   given(:question) { create(:question, user: author_of_question) }
   given!(:answer) { create(:answer, question: question, user: author_of_answer) }
@@ -15,31 +15,36 @@ I want to be able to edit my answers} do
     visit question_path(question)
     expect(page).to_not have_link 'Edit'
   end
-
-  scenario 'Author of Answer can see link Edit' do
-    sign_in(author_of_answer)
-    can_see_question(with_answers: true)
-    within '.answers' do
-      expect(page).to have_link 'edit'
+  describe 'Authenticate User' do
+    before do
+      sign_in(author_of_answer)
     end
-  end
 
-  scenario 'Author of Answer is trying edit his Answer' do
-    sign_in(author_of_answer)
-    visit question_path(question)
+    scenario 'Author of Answer can see link Edit' do
+
+
+      visit question_path(question)
+      within '.answers' do
+        expect(page).to have_link 'edit'
+      end
+    end
+
+    scenario 'Author of Answer is trying edit his Answer', js: true do
+
+      visit question_path(question)
       click_on 'edit'
-    # within '.answers' do
-    # expect(page).to_not have_link 'Save'
-    # end
+      within '.answers' do
+        expect(page).to have_button 'Save'
+      end
 
 
-  end
-
-  scenario 'Authenticate User is trying edit his not Answer' do
-    sign_in(another_authenticated_user)
-    visit question_path(question)
-    within '.answers' do
-    expect(page).to_not have_link 'Edit'
+    end
+    before { another_authenticated_user }
+    scenario 'Authenticate User is trying edit his not Answer' do
+      visit question_path(question)
+      within '.answers' do
+        expect(page).to_not have_link 'Edit'
+      end
     end
   end
 end
