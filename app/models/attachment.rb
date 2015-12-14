f427bcc8dab0a00fb8cd2cfee61434c7e860010f
Rim-777 <javatimomoss@gmail.com>
2015-12-14 13:23:01 +0200

    add test to attachments methods

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,7 @@
 class Attachment < ActiveRecord::Base
   belongs_to :attachable, polymorphic: true
   mount_uploader :file, FileUploader
+  # validates :file, presence: true
 
   def file_attached?
     !!file.file
--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -2,16 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Attachment, type: :model do
   it { should belong_to(:attachable) }
+  # it { should validate_presence_of :file }
 
+
+    let!(:attachment_with_file) { create(:attachment, file:  Rack::Test::UploadedFile.new("#{Rails.root}/spec/spec_helper.rb")) }
   describe 'method file_attached?' do
+    let!(:attachment_without_file) { create(:attachment, file: nil) }
 
-    let!(:attachment) { create(:attachment) }
-
-    it "check is file attached or not  and return false if file is not attached" do
-      expect(attachment.file_attached?).to eq false
+    it "check is file attached or not  and return false if file
+        is not attached and true if file is attached" do
+      expect(attachment_without_file.file_attached?).to eq false
+      expect(attachment_with_file.file_attached?).to eq true
     end
   end
 
-
-
+  describe 'method  file_name' do
+    it "return name of attachments' file" do
+      expect(attachment_with_file.file_name).to eq "spec_helper.rb"
+    end
+  end
 end
+
+# Rack::Test::UploadedFile.new("#{Rails.root}/spec/spec_helper.rb")
