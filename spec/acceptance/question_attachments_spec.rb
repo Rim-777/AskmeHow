58fb80a14e4da760ea6b9d035bdb6e50571e7752
Rim-777 <javatimomoss@gmail.com>
2015-12-15 22:16:21 +0200

    add acceptance for few form_fields on page

--- spec/acceptance/question_attachments_spec.rb
+++ spec/acceptance/question_attachments_spec.rb
@@ -10,15 +10,15 @@ I want to be able to attach some files to my question} do
     visit new_question_path
   end
 
-  scenario 'user can attach file when hi ask question'  do
+  scenario 'user can attach file when hi is asking question'  do
     fill_in 'question_title', with: 'Test question'
     fill_in 'question_body', with: 'Test question body'
-    attach_file 'question_attachments_attributes_0_file', "#{Rails.root}/spec/spec_helper.rb"
+    attach_file "question_attachments_attributes_0_file", "#{Rails.root}/spec/spec_helper.rb"
     click_on 'Create'
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
   end
 
-  scenario 'user is trying add few files to question', js: true do
+  scenario 'user is trying add few files to question by few file_fields', js: true do
 
     within '#question_form_new' do
       fill_in 'question_title', with: 'Question Title Test'
@@ -31,9 +31,8 @@ I want to be able to attach some files to my question} do
       click_on '[+]'
 
       all_inputs_type_file = all('input[type="file"]')
-      # sleep(1)
-      all_inputs_type_file[0].set("#{Rails.root}/spec/spec_helper.rb")
-      all_inputs_type_file[1].set("#{Rails.root}/spec/rails_helper.rb")
+      all_inputs_type_file.first.set("#{Rails.root}/spec/spec_helper.rb")
+      all_inputs_type_file.last.set("#{Rails.root}/spec/rails_helper.rb")
     end
         click_on 'question_create_button'
 
@@ -41,6 +40,33 @@ I want to be able to attach some files to my question} do
         expect(page).to have_link 'spec_helper.rb'
         expect(page).to have_link 'rails_helper.rb'
       end
+  end
+
+  scenario 'user is trying remove one of few added file_fields to question', js: true do
+
+    within '#question_form_new' do
+      fill_in 'question_title', with: 'Question Title Test'
+      fill_in 'question_body', with: 'Question Body Test'
+    end
+
+    within '.new_question' do
+      click_on '[+]'
+      click_on '[+]'
+
+      all_inputs_type_file = all('input[type="file"]')
+      all_inputs_type_file.first.set("#{Rails.root}/spec/spec_helper.rb")
+      all_inputs_type_file.last.set("#{Rails.root}/spec/rails_helper.rb")
+      expect(page).to have_link '[-]'
+
+      click_on '[-]',  match: :first
+
+    end
+    click_on 'question_create_button'
+
+    within '.question_existed_area' do
+      expect(page).to_not have_link 'spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb'
+    end
   end
 
 end
