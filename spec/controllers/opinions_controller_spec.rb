65e0935b7fdb0df12824fdbf028e51c8d009f528
Rim-777 <javatimomoss@gmail.com>
2015-12-22 17:39:00 +0200

    continue spec opinion controller

--- spec/controllers/opinions_controller_spec.rb
+++ spec/controllers/opinions_controller_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe OpinionsController, type: :controller do
-  let!(:author_of_question) { create(:user) }
-  let!(:some_user) { create(:user) }
+  let(:author_of_question) { create(:user) }
+  let(:some_user) { create(:user) }
   let!(:question) { create(:question, user: author_of_question, title: 'OldTitleText', body: 'OldBodyText') }
-  let!(:one_more_user) { create(:user) }
+  let(:one_more_user) { create(:user) }
   let!(:one_more_question) { create(:question, user: one_more_user, title: 'OldTitleText', body: 'OldBodyText') }
+  let(:one_more_some_user) { create(:user) }
 
+  describe 'PATCH #positive' do
 
-  describe 'PATCH #say_positive' do
-    before { sign_in(some_user) }
     context 'Some user is trying to say a new opinion for his not question' do
+    before { sign_in(some_user) }
 
       it 'assigns the requested question to @opinionable' do
         patch :positive, opinionable_id: question.id, opinionable_type: question.class, format: :js
@@ -30,6 +31,7 @@ RSpec.describe OpinionsController, type: :controller do
     end
 
     context 'Some user is trying to change opinion for his not question' do
+      before { sign_in(some_user) }
 
       let!(:opinion) { create(:opinion, opinionable: question, user: some_user, opinionable_type: question.class, value: -1) }
       it 'delete old opinion for this user about question if  opinion is existed and  new opinion is different ' do
@@ -46,10 +48,23 @@ RSpec.describe OpinionsController, type: :controller do
     end
 
 
-    # it 'render temlate update wiev' do
+    # it 'render template update wiev' do
     #   patch :update, id: question, question: attributes_for(:question), format: :js
     #   expect(response).to render_template :update
     # end
+
+    context 'Some one more user is trying to change  opinion of other user for his not question' do
+      let!(:one_more_opinion) { create(:opinion, opinionable: one_more_question, user: some_user,
+                                       opinionable_type: one_more_question.class, value: -1) }
+      it 'note delete opinion of other user' do
+        sign_in(one_more_some_user)
+        expect { patch :positive, opinionable_id: one_more_question,
+                       opinionable_type: one_more_question.class, format: :js}.to change(Opinion, :count).by(1)
+        expect(one_more_opinion).to_not eq nil
+        expect(one_more_opinion.value).to eq -1
+      end
+    end
+
     context 'Author of question is trying to say a new opinion for his question' do
       it 'note create new opinion' do
         sign_in(author_of_question)
@@ -62,17 +77,16 @@ RSpec.describe OpinionsController, type: :controller do
       end
     end
 
-    context 'Some one more user is trying to change  opinion of other user for his not question' do
-      let!(:one_more_opinion) { create(:opinion, opinionable: one_more_question, user: some_user,
-                                       opinionable_type: one_more_question.class, value: -1) }
-      let!(:one_more_some_user) { create(:user) }
+    context 'Some un-authenticate user is trying to say a new opinion for question' do
 
-      it 'note delete opinion of other user' do
-        sign_in(one_more_some_user)
-        expect { patch :positive, opinionable_id: one_more_question,
-                       opinionable_type: one_more_question.class, format: :js}.to change(Opinion, :count).by(1)
-        expect(one_more_opinion).to_not eq nil
-        expect(one_more_opinion.value).to eq -1
+      it 'do not create new opinion for this user about question if  opinion  do not exist' do
+        expect { patch :positive, opinionable_id: question.id,
+                       opinionable_type: question.class, format: :js }.to_not change(Opinion, :count)
+      end
+
+      it 'render nothing' do
+        patch :positive, opinionable_id: question.id, opinionable_type: question.class, format: :js
+        expect(response).to render_template nil
       end
     end
 
