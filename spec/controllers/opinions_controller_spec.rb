4df5fce8fec93a58966eb4679cba079586ecec41
Rim-777 <javatimomoss@gmail.com>
2015-12-23 11:33:14 +0200

    have supplemented opinion-controller with answer as opinionable

--- spec/controllers/opinions_controller_spec.rb
+++ spec/controllers/opinions_controller_spec.rb
@@ -1,17 +1,31 @@
 require 'rails_helper'
 
-# RSpec.describe OpinionsController, type: :controller do
-  describe OpinionsController do
-    let(:author_of_opinionable_object) { create(:user) }
-    let(:some_user) { create(:user) }
-    let(:one_more_user) { create(:user) }
-    let(:one_more_some_user) { create(:user) }
-    let!(:opinionable_object) { create(:question, user: author_of_opinionable_object, title: 'OldTitleText', body: 'OldBodyText') }
-    let!(:one_more_opinionable_object) { create(:question, user: one_more_user, title: 'OldTitleText', body: 'OldBodyText') }
+
+describe OpinionsController do
+  let(:author_of_opinionable_object) { create(:user) }
+  let(:some_user) { create(:user) }
+  let(:one_more_user) { create(:user) }
+  let(:one_more_some_user) { create(:user) }
+  let!(:opinionable_object) { create(:question, user: author_of_opinionable_object, title: 'OldTitleText', body: 'OldBodyText') }
+  let!(:one_more_opinionable_object) { create(:question, user: one_more_user, title: 'OldTitleText', body: 'OldBodyText') }
 
 
-    it_behaves_like "Opinion"
-  end
+  it_behaves_like "Opinion"
+end
 
-# end
+describe OpinionsController do
+  let(:author_of_opinionable_object) { create(:user) }
+  let(:some_user) { create(:user) }
+  let(:one_more_user) { create(:user) }
+  let(:one_more_some_user) { create(:user) }
+  let(:question) { create(:question, user: some_user) }
+
+  let!(:opinionable_object) { create(:answer, question: question,
+                                     user: author_of_opinionable_object, body: 'OldBodyText') }
+  let!(:one_more_opinionable_object) { create(:answer, question: question,
+                                              user: one_more_user, body: 'OldBodyText') }
+
+
+  it_behaves_like "Opinion"
+end
 
