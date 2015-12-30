d8f5f879ee3721dd865079fe0e72ff4d291af40d
Rim-777 <javatimomoss@gmail.com>
2015-12-31 00:23:59 +0200

    some fix

--- spec/acceptance/create_answer_comment_spec.rb
+++ spec/acceptance/create_answer_comment_spec.rb
@@ -37,38 +37,39 @@ I want to be able to  create comment } do
       expect(page).to have_content 'some comment text'
     end
   end
-  # scenario 'Un-authenticate User is trying to add comment to question', js: true do
-  #
-  #   visit question_path(question)
-  #   within '.question_existed_area .question_comments' do
-  #
-  #     fill_in "question_#{question.id}_comment_add_area", with: ''
-  #     fill_in "question_#{question.id}_comment_body_area", with: 'some comment text'
-  #
-  #
-  #     click_on "question_#{question.id}_add_comment_button"
-  #
-  #     expect(page).to_not have_content 'some comment text'
-  #     expect(page).to have_field "question_#{question.id}_comment_body_area"
-  #     expect(page).to have_button "question_#{question.id}_add_comment_button"
-  #     expect(page).to_not have_field "question_#{question.id}_add_area"
-  #   end
-  #
-  # end
-  #
-  # scenario 'User  is trying create invalid comment', js: true do
-  #
-  #   sign_in(user)
-  #   visit question_path(question)
-  #   within '.question_existed_area .question_comments' do
-  #
-  #     fill_in "question_#{question.id}_comment_add_area", with: ''
-  #     click_on "question_#{question.id}_add_comment_button"
-  #     expect(page).to have_field "question_#{question.id}_comment_body_area"
-  #     expect(page).to have_button "question_#{question.id}_add_comment_button"
-  #     expect(page).to_not have_field "question_#{question.id}_add_area"
-  #   end
-  # end
 
+
+  scenario 'Un-authenticate User is trying to add comment to question', js: true do
+
+    visit question_path(question)
+    within '.answers .answer_comments' do
+
+      fill_in "answer_#{question.id}_comment_add_area", with: ''
+      fill_in "answer_#{question.id}_comment_body_area", with: 'some comment text'
+
+
+      click_on "answer_#{question.id}_add_comment_button"
+
+      expect(page).to_not have_content 'some comment text'
+      expect(page).to have_field "answer_#{question.id}_comment_body_area"
+      expect(page).to have_button "answer_#{question.id}_add_comment_button"
+      expect(page).to_not have_field "answer_#{question.id}_add_area"
+    end
+
+  end
+
+  scenario 'User  is trying create invalid comment', js: true do
+
+    sign_in(user)
+    visit question_path(question)
+    within '.answers .answer_comments' do
+
+      fill_in "answer_#{question.id}_comment_add_area", with: ''
+      click_on "answer_#{question.id}_add_comment_button"
+      expect(page).to have_field "answer_#{question.id}_comment_body_area"
+      expect(page).to have_button "answer_#{question.id}_add_comment_button"
+      expect(page).to_not have_field "answer_#{question.id}_add_area"
+    end
+  end
 
 end
