83160f9ad1d5c16be84c1a1638c504ca01d981e7
Rim-777 <javatimomoss@gmail.com>
2016-03-07 14:18:36 +0200

    helper fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def collection_cache_key_for(model)
     klass = model.to_s.capitalize.constantize
     count = klass.count
-    max_updated_at = klass.maximum(:updated_at).try(:uts).try(:to_s)
+    max_updated_at = klass.maximum(:updated_at).try(:to_s)
     "#{model.to_s.pluralize}/collection-#{count}-#{max_updated_at}"
   end
 
