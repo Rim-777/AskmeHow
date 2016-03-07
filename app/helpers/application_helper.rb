145a11fd6f90228aabe70edac428ff9b25d81f1e
Rim-777 <javatimomoss@gmail.com>
2016-03-07 16:33:49 +0200

    caches keys fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,14 @@ module ApplicationHelper
     klass = model.to_s.capitalize.constantize
     count = klass.count
     max_updated_at = klass.maximum(:updated_at).try(:to_s)
-    "#{model.to_s.pluralize}/collection-#{count}-#{max_updated_at}"
+    "#{model.to_s.pluralize}/collection-#{count}-#{max_updated_at}-when-user-#{current_user ? current_user.id: 'no_user'}"
   end
+
+  # def object_cache_key_for(model)
+  #   klass = model.to_s.capitalize.constantize
+  #   count = klass.count
+  #   max_updated_at = klass.maximum(:updated_at).try(:to_s)
+  #   "#{model.to_s}/-#{}/-when-user-#{current_user ? current_user.id: 'no_user'}"
+  # end
 
 end
