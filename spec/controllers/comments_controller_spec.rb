876beb7d22f74a4c5e380b419b7179f3bd01a0dd
Rim-777 <javatimomoss@gmail.com>
2016-05-26 17:44:21 +0300

    refactoring of comments_controller_spec.rb

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,35 +1,40 @@
 require 'rails_helper'
 
 describe CommentsController do
-
   let!(:user) { create(:user) }
   let!(:commentable_object) { create(:question, user: user) }
   let(:comment_params) do
-    {comment: attributes_for(:comment),  commentable: 'question', commentable_type: Question, question_id: commentable_object.id  }
+    { comment: attributes_for(:comment),
+     commentable: 'question',
+     commentable_type: Question,
+     question_id: commentable_object.id }
   end
 
   let(:invalid_params) do
-    {comment: attributes_for(:invalid_comment),  commentable: 'question',   question_id: commentable_object.id  }
+    { comment: attributes_for(:invalid_comment),
+     commentable: 'question',
+     question_id: commentable_object.id }
   end
 
-  it_behaves_like "Comment"
+  it_behaves_like 'Comment'
 end
 
-
 describe CommentsController do
-
   let!(:user) { create(:user) }
   let!(:question) { create(:question, user: user) }
   let!(:commentable_object) { create(:answer, question: question, user: user) }
 
   let(:comment_params) do
-    {comment: attributes_for(:comment),  commentable: 'answer',   answer_id: commentable_object.id  }
+    { comment: attributes_for(:comment),
+     commentable: 'answer',
+     answer_id: commentable_object.id }
   end
 
   let(:invalid_params) do
-    {comment: attributes_for(:invalid_comment),  commentable: 'answer',   answer_id: commentable_object.id  }
+    { comment: attributes_for(:invalid_comment),
+     commentable: 'answer',
+     answer_id: commentable_object.id }
   end
-  it_behaves_like "Comment"
+
+  it_behaves_like 'Comment'
 end
-
-
