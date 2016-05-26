18ea4cd4807af1ed3013f76d1f274ee457bb4adc
Rim-777 <javatimomoss@gmail.com>
2016-05-26 17:29:25 +0300

    total finished refactoring attachments_controller_spec.rb

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe AttachmentsController, type: :controller do
   let!(:user) { create(:user) }
   let!(:question) { create(:question, user: user) }
-  let!(:answer) { create(:answer, question: question, user: user) }
   let!(:question_attachment) { create(:attachment, attachable: question) }
   let!(:another_user) { create(:user) }
 
@@ -17,22 +16,22 @@ RSpec.describe AttachmentsController, type: :controller do
 
     before { sign_in(user) }
 
-    context 'User is trying to remove his own attachment ' do
-      it "remove an user's answer" do
+    context 'User is trying to remove his attachment' do
+      it "remove an user's attachment" do
         expect { request }.to change(question.attachments, :count).by(-1)
+      end
+
+      it 'render to  destroy view' do
+        request
+        expect(response).to render_template :destroy
       end
     end
 
-    context 'User is trying to remove his not answer' do
-      it 'does not remove a question' do
+    context 'User is trying to remove his not attachment' do
+      it 'does not remove an attachment' do
         sign_in(another_user)
         expect { request }.to_not change(Attachment, :count)
       end
-    end
-
-    it 'render to  destroy view' do
-      request
-      expect(response).to render_template :destroy
     end
   end
 end
