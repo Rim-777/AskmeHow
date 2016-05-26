ad9708004577b238f08efe10ed631b743d1ad3c9
Rim-777 <javatimomoss@gmail.com>
2016-05-26 16:37:29 +0300

    refactoring questions_controller_spec.rb

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
-
   let(:user) { create(:user) }
-  let(:question) { create(:question, user: user, title: 'OldTitleText', body: 'OldBodyText') }
   let(:another_user) { create(:user) }
   let!(:subscriber) { create(:user) }
+  let(:question) do
+    create(:question, user: user, title: 'OldTitleText', body: 'OldBodyText')
+  end
 
   describe 'GET #index' do
-
-
     let(:questions) { create_list(:question, 2, user: user) }
 
     before do
@@ -27,12 +26,10 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #show' do
-
     before do
       sign_in(user)
       get :show, id: question
     end
-
 
     it 'assigns the requested question to @question' do
       expect(assigns(:question)).to eq question
@@ -67,15 +64,13 @@ RSpec.describe QuestionsController, type: :controller do
 
     context 'with valid attributes' do
       let(:request) { post :create, question: attributes_for(:question) }
-      let(:channel) { "/questions" }
+      let(:channel) { '/questions' }
 
       it 'save new question in database' do
-
         expect { request }.to change(Question, :count).by(1)
       end
 
       it 'save new question in database depending with user' do
-
         expect { request }.to change(user.questions, :count).by(1)
       end
 
@@ -87,76 +82,81 @@ RSpec.describe QuestionsController, type: :controller do
       it_behaves_like 'Publishable' do
         let(:message) { :publish_question }
       end
-
-
     end
 
     context 'with invalid attributes' do
+      let(:invalid_request) do
+        post :create, question: attributes_for(:invalid_question)
+      end
+
       it 'does not save the question' do
-        expect { post :create, question: attributes_for(:invalid_question)
-        }.to_not change(Question, :count)
+        expect { invalid_request }.to_not change(Question, :count)
       end
 
       it 're-render new view' do
         post :create, question: attributes_for(:invalid_question)
         expect(response).to render_template :new
       end
-
     end
   end
 
   describe 'PATCH #update' do
     before { sign_in(user) }
 
+    let(:patch_request) do
+      patch :update,
+            id: question,
+            question: { title: 'new title', body: 'new body' },
+            format: :js
+    end
+
+    let(:invalid_patch_request) do
+      patch :update,
+            id: question,
+            question: { title: 'new title', body: nil },
+            format: :js
+    end
+
     context 'User is trying update his question' do
+      before { patch_request }
 
       it 'assigns the requested question to @question' do
-        patch :update, id: question, question: attributes_for(:question), format: :js
         expect(assigns(:question)).to eq question
       end
 
       it 'changes question attributes' do
-        patch :update, id: question, question: {title: 'new title', body: 'new body'}, format: :js
         question.reload
         expect(question.title).to eq 'new title'
         expect(question.body).to eq 'new body'
       end
 
-      it 'render temlate update wiev' do
-        patch :update, id: question, question: attributes_for(:question), format: :js
+      it 'render template update wiev' do
         expect(response).to render_template :update
       end
     end
 
-
-    context 'User is trying update his question with invalid attributes' do
-
-      it 'does not change question attributes' do
-        patch :update, id: question, question: {title: 'new title', body: nil}, format: :js
-        question.reload
-        expect(question.title).to eq 'OldTitleText'
-        expect(question.body).to eq 'OldBodyText'
-      end
-    end
-
-
     context 'User is trying update his not question' do
-
       it 'does not change question attributes' do
         sign_in(another_user)
-        patch :update, id: question, question: {title: 'new title', body: 'new body'}, format: :js
+        patch_request
         question.reload
         expect(question.title).to eq 'OldTitleText'
         expect(question.body).to eq 'OldBodyText'
       end
     end
 
-
+    context 'User is trying update his question with invalid attributes' do
+      it 'does not change question attributes' do
+        invalid_patch_request
+        question.reload
+        expect(question.title).to eq 'OldTitleText'
+        expect(question.body).to eq 'OldBodyText'
+      end
+    end
   end
 
-
   describe 'DELETE # destroy' do
-
+    let(:delete_request) { delete :destroy, id: question }
     before do
       sign_in(user)
       question
@@ -164,19 +164,19 @@ RSpec.describe QuestionsController, type: :controller do
 
     context 'User is trying to delete his own question' do
       it 'remove a question' do
-        expect { delete :destroy, id: question }.to change(user.questions, :count).by(-1)
+        expect { delete_request }.to change(user.questions, :count).by(-1)
       end
     end
 
     context 'User is trying to delete  his not question' do
       it 'Does not remove a question' do
         sign_in(another_user)
-        expect { delete :destroy, id: question }.to_not change(Question, :count)
+        expect { delete_request }.to_not change(Question, :count)
       end
     end
 
     it 'redirect to  index view' do
-      delete :destroy, id: question
+      delete_request
       expect(response).to redirect_to questions_path
     end
   end
